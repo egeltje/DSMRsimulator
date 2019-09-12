844515bd1ef56939566d3fced2b6e1f7dcac14e9
Erwin Kooi <erwin@ewak.net>
2019-09-12 10:05:48 +0200

    Added an array with sample telegram

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
+/*
+ * main.cpp
+ * Contains main program code for DSMR P1 port emulator
+ * Used to test the correct functioning of P1 readers
+ */
+
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.printf("DSMR5 P1 telegram emulator");
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
 }
