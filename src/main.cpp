2e5c921dae3bae8b6d4da8fa0e0e0ad679f3f1cd
Erwin Kooi <erwin@ewak.net>
2019-09-12 19:15:36 +0200

    Added DSMR5 telegram

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,29 @@
  */
 
 #include <Arduino.h>
+#include "telegram.h"
+
+unsigned long milliseconds;
+uint8_t telegramLines;
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
   Serial.begin(115200);
   Serial.printf("DSMR5 P1 telegram emulator");
+  telegramLines = sizeof telegram50 / sizeof telegram50[0];
+  milliseconds = millis();
 }
 
 void loop() {
-
-  // put your main code here, to run repeatedly:
+  if (millis() - milliseconds > 1000) {
+    milliseconds = millis();
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    for (uint8_t i = 0; i < telegramLines; i++) {
+      Serial.printf("%s", telegram50[i]);
+    }
+    Serial.printf("\r\n"); // for debugging only
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
 }
